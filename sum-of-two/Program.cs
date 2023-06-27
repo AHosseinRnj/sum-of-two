7911f7b656b35bb1d4b7c70a103c9fad62dd5ba4
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 12:14:03 +0330

    Added Brute-force Method

--- sum-of-two/Program.cs
+++ sum-of-two/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace sum_of_two
 {
@@ -10,7 +6,30 @@ namespace sum_of_two
     {
         static void Main(string[] args)
         {
+            var input = new int[] { 2, 7, 11, 15 };
+            var target = 9;
 
+            var output = SumOfTwoBruteForce(input, target);
+
+            Console.Write("Result of Bruteforce Method: ");
+            foreach (var index in output)
+            {
+                Console.Write($"{index} ");
+            }
+        }
+
+        static int[] SumOfTwoBruteForce(int[] input, int target)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[i] + input[j] == target)
+                        return new int[] { i, j };
+                }
+            }
+
+            return new int[] { };
         }
     }
 }
