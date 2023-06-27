86b242a8ec04cfee5745ce7970ee7e53b06ae56e
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 12:34:28 +0330

    Added Two pointers Method

--- sum-of-two/Program.cs
+++ sum-of-two/Program.cs
@@ -10,8 +10,16 @@ namespace sum_of_two
             var target = 9;
 
             var output = SumOfTwoBruteForce(input, target);
-
             Console.Write("Result of Bruteforce Method: ");
+            foreach (var index in output)
+            {
+                Console.Write($"{index} ");
+            }
+
+            Console.WriteLine();
+
+            output = SumOfTwoByPointers(input, target);
+            Console.Write("Result of Two Pointers Method: ");
             foreach (var index in output)
             {
                 Console.Write($"{index} ");
@@ -27,6 +35,27 @@ namespace sum_of_two
                     if (input[i] + input[j] == target)
                         return new int[] { i, j };
                 }
+            }
+
+            return new int[] { };
+        }
+
+        // Array has to be sorted before using this Method.
+        static int[] SumOfTwoByPointers(int[] input, int target)
+        {
+            var leftPtr = 0;
+            var rightPtr = input.Length - 1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[leftPtr] + input[rightPtr] == target)
+                    return new int[] { leftPtr, rightPtr };
+
+                if (input[leftPtr] + input[rightPtr] > target)
+                    rightPtr--;
+
+                if (input[leftPtr] + input[rightPtr] < target)
+                    leftPtr++;
             }
 
             return new int[] { };
